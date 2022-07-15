78b71cdfc090ff70c8a201d9ef673136d101a681
Muhammad Arif <66298095+arfakl99@users.noreply.github.com>
2022-07-15 15:32:16 +0500

    todo list
    my Todo List

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            
+            <p>Hi this is me Arif.I am a web developer and designer.t
+                This is a simple reactjs based task where you can add all those tasks that you have to do daily.
+            you can delete all those tasks that is completed by you</p>
+        </div>
+    )
+}
